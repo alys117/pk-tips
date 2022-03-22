9e3539447a63e60e256820e6bc90a606b930c9eb
keke <alys117@163.com>
2022-03-22 17:00:10 +0800

    示例更新

---
+++ cases/使用chrome调试.js
@@ -0,0 +1,52 @@
+/* 
+启动时在 node 后面加上 --inspect-brk  标志，Node.js 将监听调试客户端，默认情况下监听在 127.0.0.1:9229 地址，
+也可以显示指定地址 --inspect-brk=_host:port_
+注意 node --inspect 与 node --inspect-brk 的区别：--inspect 不会终断，--inspect-brk 在用户代码启动之前会终断，也就是代码在第一行就会暂停执行。
+
+浏览器地址栏输入 chrome://inspect/  或者 about:inspect
+Remote Target 下展示了当前运行的 Node.js 版本号，打开 inspect 或 Open dedicated Devtools for Node 链接
+
+1、Resume script execution(F8): 恢复脚本执行，每一次都会自动执行到断点处。
+2、Step over next function call(F10)：跳过下一个函数调用，执行当前代码行，在当前代码行的下一行处停止，是一步一步的操作。
+3、Step into next function call(F11)：单步进入下一个函数调用。
+4、Step out next function call(F11)：单步退出下一个函数调用。
+5、Step(F9)：执行当前代码行并在下一行处停止。
+
+原文地址 https://www.imooc.com/article/313777
+
+node --inspect --debug-brk 使用chrome调试.js
+*/
+
+const treeData = [
+  {
+    title: '1',
+    key: '1',
+    children: [
+      {
+        title: '1-1', key: '1-1',
+        children: [
+          { title: '1-1-1', key: '1-1-1', },
+          { title: '1-1-2', key: '1-1-2', },
+        ],
+      },
+      { title: '1-2', key: '1-2', },
+      { title: '1-3', key: '1-3', },
+      { title: '1-4', key: '1-4', },
+    ],
+  },
+  { title: '2', key: '2', },
+]
+let level = 0
+function f(arr, selectedKey) {
+  return arr
+    .filter((item) => item.key !== selectedKey)
+    .map((item) => {
+      item = Object.assign({ level: level++ }, item)
+      if (item.children) {
+        item.children = f(item.children, selectedKey)
+      }
+      return item
+    })
+}
+console.log(f(treeData, '1-2'))
+console.log('end')
---
+++ oracledb/oracle.js
@@ -0,0 +1,42 @@
+var oracledb = require('oracledb');
+let Ut = require("./Delay");
+var config = {
+  user:'sdbass',　　//用户名
+  password:'ibmdb2',　　//密码
+  //IP:数据库IP地址，PORT:数据库端口，SCHEMA:数据库名称
+  connectString : "192.168.0.253:1521/sdcrm"
+};
+oracledb.getConnection(
+  config,
+  function(err, connection)
+  {
+    if (err) {
+      console.error(err.message);
+      return;
+    }
+　　//查询某表十条数据测试，注意替换你的表名
+    connection.execute("SELECT * FROM ( SELECT A.*, ROWNUM RN FROM (SELECT * FROM dim_pub_city) A WHERE ROWNUM <= 18) WHERE RN >= 0",
+      function(err, result)
+      {
+        if (err) {
+          console.error(err.message);
+          doRelease(connection);
+          return;
+        }
+        //打印返回的表结构
+        console.log(result.metaData);
+        //打印返回的行数据
+        console.log(result.rows);
+        doRelease(connection);
+      });
+  });
+
+  function doRelease(connection)
+  {
+    connection.close(
+      function(err) {
+        if (err) {
+          console.error(err.message);
+        }
+      });
+  }
