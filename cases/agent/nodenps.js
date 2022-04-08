ab6269a590031b6fd75f9131674483dc6c2ea499
keke <alys117@163.com>
2022-04-08 18:15:25 +0800

    增加原生代理，不依赖库

---
+++ cases/agent/nodenps.js
@@ -0,0 +1,11 @@
+let request = require('request');
+let util = require('util');
+request({
+    method: 'post',
+    url: 'https://www.google.com/',
+    proxy:util.format('http://%s:%d', '127.0.0.1', '7890'),
+    forever: true// 长链接
+}, (err, res) => {
+    console.log(res)
+    console.log(err)
+});
