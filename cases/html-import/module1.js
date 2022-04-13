d02aa3432cb6e9d13cac3484c1931a643bfa6b8f
keke <alys117@163.com>
2022-04-13 16:39:28 +0800

    失误提交

---
+++ cases/html-import/module1.js
@@ -0,0 +1,12 @@
+export default {
+  a:123,
+  b:()=>{
+    console.log('this is module module1.js')
+  }
+}
+export const myobj = {
+  name:'myobj hello',
+  myfunc: ()=>{
+    console.log('myfunc')
+  }
+}
