53a7a1210a29bb5c8b43348032f9ceff4692e024
keke <alys117@163.com>
2022-06-02 18:19:31 +0800

    正则汉字

---
+++ fanruan/正则匹配.js
@@ -0,0 +1,12 @@
+const reg = /[\u4e00-\u9fa5|\u3002|\uff1f|\uff01|\uff0c|\u3001|\uff1b|\uff1a|\u201c|\u201d|\u2018|\u2019|\uff08|\uff09|\u300a|\u300b|\u3008|\u3009|\u3010|\u3011|\u300e|\u300f|\u300c|\u300d|\ufe43|\ufe44|\u3014|\u3015|\u2026|\u2014|\uff5e|\ufe4f|\uffe5]{2,}/
+const str = '11113222212312312lo'
+console.log(reg.test(str) && str.length >5)
+
+
+var reg2 = /(?:[\u3400-\u4DB5\u4E00-\u9FEA\uFA0E\uFA0F\uFA11\uFA13\uFA14\uFA1F\uFA21\uFA23\uFA24\uFA27-\uFA29]|[\uD840-\uD868\uD86A-\uD86C\uD86F-\uD872\uD874-\uD879][\uDC00-\uDFFF]|\uD869[\uDC00-\uDED6\uDF00-\uDFFF]|\uD86D[\uDC00-\uDF34\uDF40-\uDFFF]|\uD86E[\uDC00-\uDC1D\uDC20-\uDFFF]|\uD873[\uDC00-\uDEA1\uDEB0-\uDFFF]|\uD87A[\uDC00-\uDFE0]){2,}/
+
+console.log(reg2.test(str));
+
+
+var regex = /^(?=.*\\d{2,})(?=.*[A-Za-z])[\\da-zA-Z]{8,}$/  // 正则表达式 至少包含两个数字和最低8位数字
+console.log(regex.test(str));
