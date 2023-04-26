dbb6f35d35d3caf92f9ec23aabe64c16a1252f4d
keke <alys117@163.com>
2023-04-27 02:51:47 +0800

    关于跨域的一些坑，包括cookie，header，还有恶心的ie11和更低版本的ie（或者说兼容性模式）

--- express-demo/cors-demo2.js
+++ express-demo/cors-demo2.js
@@ -3,10 +3,13 @@ const bodyParser=require("body-parser");
 // 解析以 application/json 和 application/x-www-form-urlencoded 提交的数据
 var jsonParser = bodyParser.json();
 var urlencodedParser = bodyParser.urlencoded({ extended: true });
+var cookieParser = require('cookie-parser');
+
 const express=require("express"); 
 var app=express();
 app.use(jsonParser)
 app.use(urlencodedParser)
+app.use(cookieParser());
 var allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
@@ -14,19 +17,36 @@ var allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Credentials','true');
   next();
 };
-// app.use(allowCrossDomain); // 可代替 cors 模块
-app.all('*', (req, res, next) => {
-  // -----跨域请求----- 可用pp.use(allowCrossDomain)代替
-  res.header('Access-Control-Allow-Origin', '*'); // res.header('Access-Control-Allow-Origin', req.get('Origin')) // 允许的地址,http://127.0.0.1:9000这样的格式
+
+app.all('/set-cookie', (req, res, next) => {
+  res.cookie("user", "jay", { maxAge: 2000000, httpOnly: true });
+  res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5502'); // res.header('Access-Control-Allow-Origin', req.get('Origin')) // 允许的地址,http://127.0.0.1:9000这样的格式
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, If-Modified-Since') // 允许跨域请求header携带哪些东西
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, If-Modified-Since, X-Token') // 允许跨域请求header携带哪些东西
+  // res.header('Access-Control-Allow-Headers', '*') // 有兼容问题,在IE下使用自定义header会报错，必须指定允许的header，例如上面的X-Token
   res.header('Access-Control-Allow-Credentials','true');
   // -----跨域请求-----
   
-  res.writeHead(200, { 'Content-Type': 'application/json;charset=utf-8' }) 
-  res.end("跨域数据")
+  res.writeHead(200, { 'Content-Type': 'application/json;charset=utf-8' })   
+  res.end("set-cookie")
 })
 
+// app.use(allowCrossDomain); // 可代替 cors 模块
+app.all('*', (req, res, next) => {
+  console.log(req);
+  // -----跨域请求----- 可用pp.use(allowCrossDomain)代替
+  res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5502'); // res.header('Access-Control-Allow-Origin', req.get('Origin')) // 允许的地址,http://127.0.0.1:9000这样的格式
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, If-Modified-Since, X-Token') // 允许跨域请求header携带哪些东西
+  // res.header('Access-Control-Allow-Headers', '*') // 有兼容问题,在IE下使用自定义header会报错，必须指定允许的header，例如上面的X-Token
+  res.header('Access-Control-Allow-Credentials','true');
+  // -----跨域请求-----
+  
+  res.writeHead(200, { 'Content-Type': 'application/json;charset=utf-8' })   
+  res.end("跨域数据")
+})
 app.listen(8000, function(){
   console.log('启动监听 8000');
 });
+
+// https://blog.csdn.net/Ed7zgeE9X/article/details/124310751
